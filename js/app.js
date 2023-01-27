e3c4995af464178d90f5706562b51b75a95416c9
“Adrian <johndoe@example.com>
2023-01-27 17:59:55 +0100

    cambios ficheros

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const App = {
+    name: 'barrels',
+    description: 'basic canvas app for Ironhack Project',
+    version: '1.0.0',
+    license: undefined,
+    author: ' Adrian and Aldi',
+    canvasTag: undefined,
+    ctx: undefined,
+    framesIndex: 0,
+
+
+}
